47e9f9211ce8bfd4241d650328ca1c032f447827
unknown <valclemir15@hotmail.com>
2020-04-03 23:02:12 -0300

    Primeiro commit

---
+++ service_ans/config_db.py
@@ -0,0 +1,34 @@
+import json 
+
+class Config:
+
+    
+    with open(r'C:\Users\valclemir\Documents\Azure-estudos\ANS\service_ans\config.json', 'r') as read_json:
+        config = json.load(read_json)
+
+    # Database config
+    db_host = config['db_config']['host']
+    db_domain = config['db_config']['domain']
+    db_user = config['db_config']['user']
+    db_password = config['db_config']['password']
+    db_name = config['db_config']['database']
+
+    # Mail config 
+    ME = config['mail_config']['ME']
+    TO = config['mail_config']['TO']
+
+    #FTP config 
+    ftp_host = config['ftp_config']['host']
+    ftp_user = config['ftp_config']['user']
+    ftp_password = config['ftp_config']['password']
+    ftp_folder = config['ftp_config']['local_folder_download']
+    
+
+    path = 'repository'
+
+
+
+    
+    
+    
+
---
+++ service_ans/send_mail.py
@@ -0,0 +1,99 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from config_db import Config
+
+
+class SendMail:
+    def mountHtmlMail(self, DF):
+            TEXT = """
+                <!DOCTYPE html>
+                        <html>
+                        
+                        <head>
+                        <style>
+                        #titulo {
+                            font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+                        }
+                        #ANS {
+                        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+                        border-collapse: collapse;
+                        width: 50%;
+                        }
+
+                        #ANS td, #ANS th {
+                        border: 1px solid #ddd;
+                        padding: 8px;
+                        }
+
+                        #ANS tr:nth-child(even){background-color: #f2f2f2;}
+
+                        #ANS tr:hover {background-color: #ddd;}
+
+                        #ANS th {
+                        padding-top: 12px;
+                        padding-bottom: 12px;
+                        text-align: left;
+                        background-color: #4CAF50;
+                        color: white;
+                        }
+                        </style>
+                        </head>
+                        <body>
+                        <H1 id="titulo">Processamento Arquivo ANS</H1>
+                        <table id="ANS">
+                        <tr style="background-color: blue;color: white;">
+                            <th>COMPETÊNCIA</th>
+                            <th>LINHAS PROCESSADAS</th>
+                            
+                        </tr>
+                        <tr>
+                            <td>"""+str(DF['COMPETENCIA'].values[0])+"""</td>
+                            <td>"""+str(DF.shape[0])+"""</td>
+                            
+                        </tr>
+                       
+                        </table>
+
+                        </body>
+                        </html>
+
+
+
+            """
+            return TEXT
+
+
+
+    def sendMail(self, DF):
+        try: 
+            ME = Config.ME
+            TO = Config.TO
+
+            #Cria um container de menssagem
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = "PROCESSAMENTO ARQUIVO ANS"
+            msg['From'] = ME
+            msg['To'] = TO
+
+            header = "PROCESSAMENTO ARQUIVO ANS"
+            # Cria o titulo e corpo HTML
+            TITLE = MIMEText(header, 'plain')
+            BODY = MIMEText(SendMail().mountHtmlMail(DF), 'html')
+
+            msg.attach(TITLE)
+            msg.attach(BODY)
+            # Servidor de envio de email
+            #mail = smtplib.SMTP('172.18.88.33', 25) 
+            mail = smtplib.SMTP('smtp.gmail.com: 587')
+
+            mail.ehlo()
+            mail.starttls()
+            mail.mail(ME)
+            mail.login('valclemor@gmail.com', 'heller123')
+            mail.sendmail(ME, TO, msg.as_string())
+            mail.quit()
+
+        except Exception as e:
+            print(e)
+
